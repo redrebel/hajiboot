c19043330ee53ea1917abc7ec5f23eca22976980
Lee junggeun <cjred77@gmail.com>
2015-08-04 13:31:38 +0900

    add App.java

---
+++ hajiboot-jdbc/src/main/java/com/example/App.java
@@ -0,0 +1,37 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.example.domain.Customer;
+import com.example.repository.CustomerRepository;
+
+/**
+ * Hello world!
+ *
+ */
+@EnableAutoConfiguration
+@ComponentScan
+public class App implements CommandLineRunner
+{
+	@Autowired
+	CustomerRepository customerRepository;
+	
+    public static void main( String[] args )
+    {
+        SpringApplication.run(App.class, args);
+    }
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		//데이터 추
+		Customer created = customerRepository.save(new Customer(
+				null, "Hidetoshi", "Dekisugi"));
+		System.out.println(created + " is created!");
+		
+		customerRepository.findAll().forEach(System.out::println);
+	}
+}
